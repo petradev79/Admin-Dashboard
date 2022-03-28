3106270efcd856f50c92cff85d6303ddf83298c7
Ivan Petrovic <petraivan1010@gmail.com>
2022-03-28 22:02:22 +0200

    Add firebase auth  initial commit

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,17 +1,19 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.,
-  authDomain: 'admin-dashboard-a5178.firebaseapp.com',
-  projectId: 'admin-dashboard-a5178',
-  storageBucket: 'admin-dashboard-a5178.appspot.com',
-  messagingSenderId: '118121100229',
-  appId: '1:118121100229:web:841cf5016e3c9eaf3e91ac',
-  measurementId: 'G-V4NQSN1KV0',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,35 @@
+// import { useState } from 'react';
 import './login.scss';
 
 const Login = () => {
-  return <div className='Login'>Login</div>;
+  // const [loginEmail, setLoginEmail] = useState('');
+  // const [loginPassword, setLoginPassword] = useState('');
+
+  // const handleLogin = (e: { preventDefoult: () => void; }) => {
+  //   e.preventDefoult();
+  //   login(loginEmail, loginPassword)
+
+  // };
+
+  return (
+    <div className='Login'>
+      <h3> Login </h3>
+      {/* <input
+        placeholder='Email...'
+        onChange={event => {
+          setLoginEmail(event.target.value);
+        }}
+      />
+      <input
+        placeholder='Password...'
+        onChange={event => {
+          setLoginPassword(event.target.value);
+        }}
+      /> */}
+
+      {/* <button onSubmit={handleLogin}> Login</button> */}
+    </div>
+  );
 };
 
 export default Login;
