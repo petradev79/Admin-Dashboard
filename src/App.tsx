4cb836530fab8d9eaba40c98f2511c3d1e4008bc
Ivan Petrovic <ivan.petrovic@ingsoftware.com>
2022-03-31 08:51:17 +0200

    refacture auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { db } from './firebase/firebase-config';
-import { collection, getDocs } from '@firebase/firestore';
+// import { collection, getDocs } from '@firebase/firestore';
 import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './pages/home/Home';
@@ -11,43 +11,43 @@ import New from './pages/new/New';
 import Single from './pages/single/Single';
 import './styles/main.scss';
 
-type transactionType = {
-  id: string;
-  name: string;
-  date: Date;
-  amount: number;
-  status: string;
-  type: string;
-  description: string;
-  address?: string;
-};
+// type transactionType = {
+//   id: string;
+//   name: string;
+//   date: Date;
+//   amount: number;
+//   status: string;
+//   type: string;
+//   description: string;
+//   address?: string;
+// };
 
 const App = () => {
-  const [transactions, setTransactions] = useState<transactionType[]>([]);
+  // const [transactions, setTransactions] = useState<transactionType[]>([]);
 
   // const login = async () => {};
 
   // const logout = async () => {};
 
-  const transactionsCollectionRef = collection(db, 'transactions');
+  // const transactionsCollectionRef = collection(db, 'transactions');
 
-  useEffect(() => {
-    const getTransactions = async () => {
-      const data = await getDocs(transactionsCollectionRef);
-      setTransactions(
-        data?.docs.map((doc: { data: () => any; id: string }) => ({
-          ...doc.data(),
-          id: doc.id,
-        }))
-      );
-    };
-    try {
-      getTransactions();
-    } catch (err: any) {
-      console.log(err.message);
-    }
-  }, []);
-  console.log(transactions);
+  // useEffect(() => {
+  //   const getTransactions = async () => {
+  //     const data = await getDocs(transactionsCollectionRef);
+  //     setTransactions(
+  //       data?.docs.map((doc: { data: () => any; id: string }) => ({
+  //         ...doc.data(),
+  //         id: doc.id,
+  //       }))
+  //     );
+  //   };
+  //   try {
+  //     getTransactions();
+  //   } catch (err: any) {
+  //     console.log(err.message);
+  //   }
+  // }, []);
+  // console.log(transactions);
 
   return (
     <div className='app'>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ProvideAuth } from './context/use-auth.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProvideAuth>
+      <App />
+    </ProvideAuth>
   </React.StrictMode>,
   document.getElementById('root')
 );
